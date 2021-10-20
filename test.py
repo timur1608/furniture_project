b23c2256e5d8fa79582016930c3ae045d0b656aa
Тимур Измайлов <stim20045@list.ru>
2021-10-20 19:32:43 +0500

    downloading photos from ikea with this script

---
+++ test.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib
+import pandas as pd
+import lxml
+
+
+def get_photo(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('img', class_='range-revamp-aspect-ratio-image__image')
+    if quotes:
+        jpg = urllib.request.urlopen(quotes[0]['src']).read()
+        return jpg
+    else:
+        return
+
+
+if __name__ == '__main__':
+    csv_file = pd.read_csv('db/IKEA_SA_Furniture_Web_Scrapings_sss.csv', index_col=0)
+    array = csv_file[csv_file['category'] == 'Sofas & armchairs']['link']
+    for i, j in zip(range(len(array)), array):
+        output_image = f'sofas_and_armchairs/output{array.index[i]}.png'
+        file = open(output_image, 'wb')
+        photo = get_photo(j)
+        if photo:
+            file.write(photo)
+        file.close()
